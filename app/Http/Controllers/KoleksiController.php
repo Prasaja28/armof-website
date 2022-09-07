db5624657cf21c4f7fba495d3fc8cb3df6babc3f
Mukhammad Nur Prasaja <prasaja55@gmail.com>
2022-09-07 22:35:10 +0700

    update

--- app/Http/Controllers/KoleksiController.php
+++ app/Http/Controllers/KoleksiController.php
@@ -13,16 +13,16 @@ class KoleksiController extends Controller
     {
         $koleksi = Koleksi::select(
             'koleksi.*',
-            'Kategori_furniture.nama_kategori_furniture as furniture_name',
-            'Kategori_fungsi.nama_kategori_fungsi as fungsi_name',
+            'kategori_furniture.nama_kategori_furniture as furniture_name',
+            'kategori_fungsi.nama_kategori_fungsi as fungsi_name',
         )
 
-            ->leftjoin('Kategori_furniture', 'koleksi.furniture_id', '=', 'Kategori_furniture.id')
-            ->leftjoin('Kategori_fungsi', 'koleksi.fungsi_id', '=', 'Kategori_fungsi.id')
+            ->leftjoin('kategori_furniture', 'koleksi.furniture_id', '=', 'kategori_furniture.id')
+            ->leftjoin('kategori_fungsi', 'koleksi.fungsi_id', '=', 'kategori_fungsi.id')
             ->get();
 
-        $furnitureKol = DB::table('Kategori_furniture')->orderBy('nama_kategori_furniture', 'asc')->get();
-        $fungsiKol = DB::table('Kategori_fungsi')->orderBy('nama_kategori_fungsi', 'asc')->get();
+        $furnitureKol = DB::table('kategori_furniture')->orderBy('nama_kategori_furniture', 'asc')->get();
+        $fungsiKol = DB::table('kategori_fungsi')->orderBy('nama_kategori_fungsi', 'asc')->get();
         return view('admin.admin-koleksi.index', compact('koleksi', 'furnitureKol', 'fungsiKol'));
     }
 
